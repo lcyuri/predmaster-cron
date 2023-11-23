3f38961caa6fcd4febf597941c507a3f6e914986
yleorat <yuri.leorato@adp.com>
2023-11-23 15:17:58 -0300

    [Cron] - Adjusting code after first real test

--- src/index.ts
+++ src/index.ts
@@ -9,14 +9,17 @@ const writeFileAsync = util.promisify(fs.writeFile);
 dotenv.config();
 
 // Main
-cron.schedule('*/1 * * * *', async () => {
+cron.schedule('0 */2 * * *', async () => {
   try {
     await alarmCronJob();
-    //await historyCronJob();
+    console.log('');
+    // await historyCronJob();
     await previsionCronJob();
+    console.log('');
     await settingsCronJob();
+    console.log('');
   } catch (error) {
-    console.log('Error running main task - ', error);
+    console.error('cron - ', error);
   }
 });
 
@@ -27,17 +30,15 @@ const alarmCronJob = async () => {
 
     let lastLine = await getLastLine('yellowAlarm');
     let newLines = await getNewLines(process.env.YELLOW_ALARM_PATH, lastLine);
-    let url = process.env.HOST_NAME + process.env.ALARM_ENDPOINT + `?clientId=${process.env.CLIENT_ID}&color=yellow`;
-    await postNewLines(url, newLines);
-    await updateLastLine('yellowAlarm', newLines[0]);
+    let url = getUrl('yellowAlarm');
+    await handleLines(url, newLines, 'yellowAlarm');
 
     lastLine = await getLastLine('redAlarm');
     newLines = await getNewLines(process.env.RED_ALARM_PATH, lastLine);
-    url = process.env.HOST_NAME + process.env.ALARM_ENDPOINT + `?clientId=${process.env.CLIENT_ID}&color=red`;
-    await postNewLines(url, newLines);
-    await updateLastLine('redAlarm', newLines[0]);
+    url = getUrl('redAlarm');
+    await handleLines(url, newLines, 'redAlarm');
 
-    console.log('Alarm cron job is complete');
+    console.log('Alarm cron job is complete at: ', new Date());
   } catch(error) {
     console.log('Error running alarm cron job - ', error);
   }
@@ -65,13 +66,13 @@ const previsionCronJob = async () => {
 
     const lastLine = await getLastLine('prevision');
     const newLines = await getNewLines(process.env.PREVISION_PATH, lastLine);
-    const url = process.env.HOST_NAME + process.env.PREVISION_ENDPOINT + `?clientId=${process.env.CLIENT_ID}`;
-    await postNewLines(url, newLines);
-    await updateLastLine('prevision', newLines[0]);
+    const url = getUrl('prevision');
+    await handleLines(url, newLines, 'prevision');
 
-    console.log('Prevision cron job is complete');
+    console.log('Prevision cron job is complete at: ', new Date());
   } catch(error) {
-    console.log('Error running prevision cron job - ', error);
+    console.error('previsionCronJob - ', error);
+    throw new Error('Error running prevision cron job');
   }
 }
 
@@ -79,60 +80,101 @@ const settingsCronJob = async () => {
   try {
     console.log('Running settings cron job');
 
-    const lastLine = await getLastLine(process.env.SETTINGS_PATH);
+    const lastLine = await getLastLine('settings');
     const newLines = await getNewLines(process.env.SETTINGS_PATH, lastLine);
-    const url = process.env.HOST_NAME + process.env.SETTINGS_ENDPOINT + `?clientId=${process.env.CLIENT_ID}`;
-    await postNewLines(url, newLines);
-    await updateLastLine('settings', newLines[0]);
+    const url = getUrl('settings');
+    await handleLines(url, newLines, 'settings');
 
-    console.log('Settings cron job is complete');
+    console.log('Settings cron job is complete at: ', new Date());
   } catch(error) {
-    console.log('Error running settings cron job - ', error);
+    console.error('settingsCronJob - ', error);
+    throw new Error('Error running settings cron job');
   }
 }
 
 // Utils and services
 export const getLastLine = async (key: string): Promise<string> => {
   try {
-    const data = await readFileAsync(process.env.LAST_LINE_PATH, 'utf8');
+    const data = await readFileAsync(process.env.LAST_LINES_PATH, 'utf8');
     const json = JSON.parse(data);
     return json[key];
   } catch (error) {
-    console.log('Error getting last line - ', error);
-  }
-}
-
-export const updateLastLine = async (key: string, value: string): Promise<void> => {
-  try {
-    const data = await readFileAsync(process.env.LAST_LINE_PATH, 'utf8');
-    const json = JSON.parse(data);
-    json[key] = value;
-    await writeFileAsync(process.env.LAST_LINE_PATH, JSON.stringify(json), 'utf8');
-  } catch (error) {
-    console.log('Error updating last line - ', error);
+    console.error(`getLastStoredLine - `, error);
+    throw new Error('Error getting last line stored');
   }
 }
 
 export const getNewLines = async (path: string, lastLine: string): Promise<string[]> => {
   try {
     const data = await readFileAsync(path, 'utf8');
-    const lines = data.split('\n');
-    return lines.filter(line => line !== lastLine);
+    let lines = data.split('\n');
+    lines = removeEmptyLines(lines);
+    const newLines = removeExistingLines(lines, lastLine);
+    return newLines;
   } catch (error) {
-    console.log('Error getting new lines - ', error);
+    console.error(`getNewLines - `, error);
+    throw new Error('Error getting new lines');
   }
 }
 
-const postNewLines = async (url: string, newLines: string[]): Promise<void> => {
-  const requests = newLines.map(async (line) => {
-    try {
-      await axios.post(url, { line });
-      return line;
-    } catch (error) {
-      console.error(`Error sending line to API: ${line}`, error);
-      return line;
-    }
-  });
+export const removeExistingLines = (lines: string[], lastLine: string) => {
+  const lastIndex = lines.indexOf(lastLine);
+  if (lastIndex === -1) {
+    return lines;
+  } else {
+    return lines.slice(lastIndex + 1);
+  }
+}
 
-  await Promise.all(requests);
+export const removeEmptyLines = (lines: string[]) => {
+  return lines.filter(line => line.trim().length > 0);
+}
+
+const getUrl = (key: string): string => {
+  switch (key) {
+    case 'prevision':
+      return process.env.HOST_NAME + process.env.PREVISION_ENDPOINT + `?clientId=${process.env.CLIENT_ID}`;
+    case 'settings':
+      return process.env.HOST_NAME + process.env.SETTINGS_ENDPOINT + `?clientId=${process.env.CLIENT_ID}`;
+    case 'yellowAlarm':
+      return process.env.HOST_NAME + process.env.ALARM_ENDPOINT + `?clientId=${process.env.CLIENT_ID}&color=yellow`;
+    case 'redAlarm':
+      return process.env.HOST_NAME + process.env.ALARM_ENDPOINT + `?clientId=${process.env.CLIENT_ID}&color=red`;
+    default:
+      return '';
+  }
+}
+
+async function handleLines(url: string, newLines: string[], job: string): Promise<void> {
+  for (const line of newLines) {
+    try {
+      const response = await postNewLine(url, line);
+      if (response.data?.length > 0) {
+        await updateLastLine(line, job);
+      }
+    } catch (error) {
+      console.error('processLines - ', error);
+    }
+  }
+}
+
+const postNewLine = async (url: string, newLine: string): Promise<any> => {
+  try {
+    return await axios.post(url, { lines: newLine });
+  } catch (error) {
+    console.error(`postNewLines - `, error);
+    throw new Error('Error posting new lines');
+  }
+}
+
+export const updateLastLine = async (value: string, key: string): Promise<void> => {
+  try {
+    const data = await readFileAsync(process.env.LAST_LINES_PATH, 'utf8');
+    const json = JSON.parse(data);
+    json[key] = value || '';
+    await writeFileAsync(process.env.LAST_LINES_PATH, JSON.stringify(json), 'utf8');
+  } catch (error) {
+    console.error(`updateLastLine - `, error);
+    throw new Error('Error updating last line');
+  }
 }
